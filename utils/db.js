036fa8ce10614c868313781c66e6fc97b649d9e1
lonelynight-dark <52921466+trantrunghieu0409@users.noreply.github.com>
2022-06-21 11:09:47 +0700

    update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,6 +3,7 @@ import kn from 'knex';
 export const connectionInfo = {
   host: 'eu-cdbr-west-02.cleardb.net',
   user: 'bd14ed0b560fda',
+  port: '3306',
   password: '3d05c9e9',
   database: 'heroku_0d0dfdaeef9822b'
 };
