04b3032a9c4581604ea5b377bb7ee88d96ca9cf0
nvtphong-Executionlab <phong.nguyen@executionlab.asia>
2022-06-22 22:21:32 +0700

    fix bugs

--- unit_test.js
+++ unit_test.js
@@ -28,7 +28,6 @@ viewMdware(app);
 routesMdware(app);
 
 const port = 3000;
-// const server = createServer(app).listen(port);
 
 const server = app.listen(port, () => {
     console.log(`Listening on port http://localhost:${port}`);
